4818f13d69103a5726f9e85effafa71f9425339c
Lukas Martini <hello@lutoma.org>
2017-07-21 17:54:59 +0200

    Add background to keywords in subtitles, slightly slant first area

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
 	var typed = new Typed('#subtitle', {
-		strings: [
-			"We want a Europe where sharing, innovation and copyright go hand in hand.",
-			"We want a Europe where madness and insanity prevail!",
-			"Help I'm trapped in a campaign website!",
-			"bleep bloop this is another string. Computers, how do they work."],
+		stringsElement: '#subtitle-strings',
 		typeSpeed: 30
 	});
 })

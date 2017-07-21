e95ffd64733c6fc4f48b46b1b21d3475a6bfb0d4
Lukas Martini <hello@lutoma.org>
2017-07-21 15:04:35 +0200

    Add 'get involved' button to start area and make subtitle typed.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+	var typed = new Typed('#subtitle', {
+		strings: [
+			"We want a Europe where sharing, innovation and copyright go hand in hand.",
+			"We want a Europe where madness and insanity prevail!",
+			"Help I'm trapped in a campaign website!",
+			"bleep bloop this is another string. Computers, how do they work."],
+		typeSpeed: 30
+	});
+})

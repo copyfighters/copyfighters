c8404e424c41811402e8d010ac71c215e310c971
PeterTheOne <petertheone@gmail.com>
2017-07-22 16:48:13 +0200

    add activist model

--- copyfight/urls.py
+++ copyfight/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+from campaign import views
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
-	url(r'^$', TemplateView.as_view(template_name='index.html')),
+	url(r'^$', views.index),
+	url(r'^formtest/', views.get_name_email),
 ]

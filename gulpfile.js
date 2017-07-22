6fd9a4d2a32842d1e805b1c4a441915b6ddeb291
Daniel Waxweiler <daniel.waxweiler@gmail.com>
2017-07-22 15:47:43 +0200

    remove gulp copy bootstrap command

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,6 @@ var gulp = require('gulp');
 
 var sass = require('gulp-sass');
 
-// Copy Bootstrap files to static folders.
-gulp.task('copy-bootstrap', function() {
-  // CSS
-  css = [
-    './node_modules/bootstrap/dist/css/bootstrap.min.css',
-    './node_modules/bootstrap/dist/css/bootstrap.min.css.map',
-  ];
-  gulp.src(css)
-    .pipe(gulp.dest('./static/css'));
-
-  // JavaScript
-  gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js')
-    .pipe(gulp.dest('./static/js/vendor'));
-});
-
 // Convert SASS to CSS files.
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')

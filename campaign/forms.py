8aecb905415a0991986c0af475d93cf041f6622f
PeterTheOne <petertheone@gmail.com>
2017-07-23 15:18:42 +0200

    form: make fields required

--- campaign/forms.py
+++ campaign/forms.py
@@ -9,6 +9,12 @@ class ActivistForm(ModelForm):
 
 
 class ActivistEmailForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ActivistEmailForm, self).__init__(*args, **kwargs)
+        for field in self.Meta.required_fields:
+            self.fields[field].required = True
+
     class Meta:
         model = models.Activist
         fields = ['first_name', 'country', 'email']
+        required_fields = ['first_name', 'country', 'email']

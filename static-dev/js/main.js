6e2f0a2a8388650d7c97d8d650e1f11fc89fcfcc
Daniel Waxweiler <daniel.waxweiler@gmail.com>
2017-07-31 17:37:13 +0200

    shuffle and select among a few issue statements & reddit points to issue anchor

--- static-dev/js/main.js
+++ static-dev/js/main.js
@@ -10,6 +10,52 @@ $(document).ready(function(){
     $(this).html(event.strftime(
 			'<td>%D</td><td>%H</td><td>%M</td><td>%S</td>'));
   });
+
+	// Initialise sharers.
+	setSelectOptionRandomly('issue-1');
+	setSelectOptionRandomly('issue-2');
+	setSelectOptionRandomly('issue-3');
+});
+
+function randomNumberFromInterval(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function setSelectOptionRandomly(id)
+{
+	var selectElement = $('#' + id + '-message-select');
+	var numberOptions = selectElement.children('option').length;
+	var optionNumber = randomNumberFromInterval(1, numberOptions);
+	selectElement.prop('selectedIndex', optionNumber - 1);
+	updateLinks(selectElement, id);
+}
+
+function updateLinks(selectElement, id)
+{
+	var selectedIndex = selectElement.prop('selectedIndex');
+	var text = selectElement.find('option[value="' + (selectedIndex + 1) + '"]')
+		.text();
+	$('.' + id + ' .share > .facebook').prop('href',
+		'https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fcopyfighters.eu&quote='
+		+ encodeURIComponent(text)
+	);
+	$('.' + id + ' .share > .twitter').prop('href',
+		'https://twitter.com/home?status='
+		+ encodeURIComponent(text + ' https://copyfighters.eu')
+	);
+}
+
+$('#issue-1-message-select').change(function() {
+	updateLinks($(this), 'issue-1');
+});
+
+$('#issue-2-message-select').change(function() {
+	updateLinks($(this), 'issue-2');
+});
+
+$('#issue-3-message-select').change(function() {
+	updateLinks($(this), 'issue-3');
 });
 
 $('#learnmore-link').click(function() {

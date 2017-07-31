d04930e9fdb90e55f3444f2be28b226e769bfd97
Daniel Waxweiler <daniel.waxweiler@gmail.com>
2017-07-31 12:33:34 +0200

    optimise SASS & put read more button on next line & fix small bugs

--- static/js/main.js
+++ static/js/main.js
@@ -12,18 +12,21 @@ $('#learnmore-link').click(function() {
 
 $('.area-2 .btn.show-details').click(function() {
 	$('.area-2 div:nth-child(2)').toggle();
+	$('.area-2 div:nth-child(3)').toggle();
 	var label = $(this).text();
 	$(this).text(label == 'View details ↓' ? 'Hide details ↓' : 'View details ↓');
 });
 
 $('.area-3 .btn.show-details').click(function() {
 	$('.area-3 div:nth-child(2)').toggle();
+	$('.area-3 div:nth-child(3)').toggle();
 	var label = $(this).text();
 	$(this).text(label == 'View details ↓' ? 'Hide details ↓' : 'View details ↓');
 });
 
 $('.area-4 .btn.show-details').click(function() {
 	$('.area-4 div:nth-child(2)').toggle();
+	$('.area-4 div:nth-child(3)').toggle();
 	var label = $(this).text();
 	$(this).text(label == 'View details ↓' ? 'Hide details ↓' : 'View details ↓');
 });

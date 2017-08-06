11854aeb11eb272ed651d1d5af5cf7d037b35e8a
Daniel Waxweiler <daniel.waxweiler@gmail.com>
2017-08-06 22:20:42 +0200

    add communications toolkit page

--- copyfight/urls.py
+++ copyfight/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
 	url(r'^$', views.index),
 	url(r'^formtest/', views.get_name_email),
 	url(r'^email_form/', views.get_email),
+	url(r'^communications-toolkit/', TemplateView.as_view(template_name = 'communications-toolkit.html'), name = 'communications-toolkit'), 
 	url(r'^imprint/', TemplateView.as_view(template_name = 'imprint.html'), name = 'imprint'),
 	url(r'^privacy/', TemplateView.as_view(template_name = 'privacy.html'), name = 'privacy'),
 	url(r'^outbound/', views.outbound, name='outbound'),

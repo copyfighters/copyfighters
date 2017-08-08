89480152ae8ab06c98e8a98a5fa5a07d2b288171
Daniel Waxweiler <daniel.waxweiler@gmail.com>
2017-08-08 16:44:20 +0200

    rename resources to references and communications-toolkit to toolkit

--- copyfight/urls.py
+++ copyfight/urls.py
@@ -4,16 +4,16 @@ from django.views.generic.base import TemplateView
 from campaign import views
 
 urlpatterns = [
-	url(r'^admin/', admin.site.urls),
 	url(r'^$', views.index),
-	url(r'^formtest/', views.get_name_email),
+	url(r'^admin/', admin.site.urls),
 	url(r'^email_form/', views.get_email),
-	url(r'^communications-toolkit/', TemplateView.as_view(template_name = 'communications-toolkit.html'), name = 'communications-toolkit'), 
-	url(r'^imprint/', TemplateView.as_view(template_name = 'imprint.html'), name = 'imprint'),
-	url(r'^privacy/', TemplateView.as_view(template_name = 'privacy.html'), name = 'privacy'),
-	url(r'^outbound/', views.outbound, name='outbound'),
-	url(r'^resources/', TemplateView.as_view(template_name = 'resources.html'), name = 'resources'),
+	url(r'^formtest/', views.get_name_email),
 	url(r'^i18n/setlang/(?P<language>[a-zA-Z\-]+)/', views.switch_language, name = 'switch-language'),
 	url(r'^i18n/', include('django.conf.urls.i18n')),
+	url(r'^imprint/', TemplateView.as_view(template_name = 'imprint.html'), name = 'imprint'),
+	url(r'^outbound/', views.outbound, name='outbound'),
+	url(r'^privacy/', TemplateView.as_view(template_name = 'privacy.html'), name = 'privacy'),
+	url(r'^references/', TemplateView.as_view(template_name = 'references.html'), name = 'references'),
+	url(r'^toolkit/', TemplateView.as_view(template_name = 'toolkit.html'), name = 'toolkit'),
 
 ]
